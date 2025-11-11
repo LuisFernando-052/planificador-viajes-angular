f5b04873702c8385db416ca765dafcf2e4a1aa41
Luis-Fernando <luisfernandoquispelizunde052@gmail.com>
2025-11-11 18:30:42 -0500

    Configuración inicial de Firebase (Auth y Firestore)

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+  apiKey: "AIzaSyAOiOxX9ZP8Yh3uPi6zw9b8qtG6pNX3BWU",
+  authDomain: "planificador-viajes-87c70.firebaseapp.com",
+  projectId: "planificador-viajes-87c70",
+  storageBucket: "planificador-viajes-87c70.firebasestorage.app",
+  messagingSenderId: "830935249591",
+  appId: "1:830935249591:web:f9a2db4c600250fcb29332"
+  }
+};
